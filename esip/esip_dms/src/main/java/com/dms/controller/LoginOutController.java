e480e583a4a49449603ebe4a09176ff7559892e3
jimmy <550705935@qq.com>
2018-07-09 16:40:45 +0800

    切换到dev分支的代码

--- esip/esip_dms/src/main/java/com/dms/controller/LoginOutController.java
+++ esip/esip_dms/src/main/java/com/dms/controller/LoginOutController.java
@@ -7,18 +7,7 @@ public class LoginOutController {
 	public void setLoginOut(TdCustomer customer){
 		
 		customer.setPhoneNo("110");
-		String sql = "";
 		
-		String str = "hello World";
-		
-		String str2 ="测试人员2改变了代码";
-		
-		String hello = "hello  Java";
-		
-		String str1 = "测试人员1改变了";
-		
-		String str11 = "开发代码";
-		
-		String str12 = "开发了ajax后台";
+		String str =  "切换到dev分支的代码";
 	}
 }
