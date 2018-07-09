a63145465aff3c63a102aba2eb38b902195b8fbd
jimmy <550705935@qq.com>
2018-07-09 17:34:39 +0800

    代码提交到devo分支

--- esip/esip_dms/src/main/java/com/dms/controller/LoginOutController.java
+++ esip/esip_dms/src/main/java/com/dms/controller/LoginOutController.java
@@ -8,8 +8,9 @@ public class LoginOutController {
 		
 		customer.setPhoneNo("110");
 		
-		String str =  "切换到dev分支的代码";
 		
-		String  str1 = "切换分支后的提交";
+		
+		String str11 = "devo分支devo分支";
+		
 	}
 }
